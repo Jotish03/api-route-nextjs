472d31cdc1ea30219d0dd6e34304cdddf4a7acdb
Jotish Sharma <jotishsharma03@gmail.com>
2024-01-25 18:44:04 +0530

    initial route

--- app/api/route.js
+++ app/api/route.js
@@ -1,8 +1,14 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: "OK" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+export async function POST(request) {
+    if(request.method === 'POST'){
+      const email = request.body.email
+      const feedbackText = request.body.text
+
+      const newFeedback = {
+        id:new Date().toISOString(),
+        email:email,
+        text:feedbackText
+      }
+    }
+
+    const filePath = 
 }
